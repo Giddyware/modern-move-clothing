de585f9d4563121927c4cc0f008b24468080abf7
Giddy <Idowugidiyanuoluwa@gmail.com>
2021-04-29 16:17:08 +0100

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-const JeanPage = () => {
-  <div>
-    <h1>Jean Page</h1>
-    <h1>Jean Page</h1>
-    <h1>Jean Page</h1>
-  </div>;
-};
+import ShopPage from './pages/shop/shop.component.jsx'
+
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/jean" component={JeanPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
