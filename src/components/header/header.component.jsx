1ba6fd89a2c5233bff53662339d5e67579d2fb48
Giddy <Idowugidiyanuoluwa@gmail.com>
2021-05-16 19:49:49 +0100

    implemented firebase utils, included ability to store authenticated user into our firestore database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -21,11 +21,12 @@ const Header = ({ currentUser }) => {
         </Link>
         {currentUser ? (
           <div className="option" onClick={() => auth.signOut()}>
-          
             SIGN OUT
           </div>
         ) : (
-          <Link className="option" to='/signin'> SIGN IN</Link>
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
         )}
       </div>
     </div>
